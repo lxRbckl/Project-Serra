6bab87740a761923b60673c782b451b3bca656f2
highlander <alexarbuckle@protonmail.com>
2021-08-15 00:32:00 -0500

    version update - universal add function - weight function - UI is up and running

--- Source/Back.py
+++ Source/Back.py
@@ -1,51 +1,51 @@
 # Import <
+from os import getcwd
 from discord import Intents
+from datetime import datetime as dt
+from Serra import jsonLoad, jsonDump
 from discord.ext.commands import Bot
 
 # >
 
 
 # Declaration
+path = getcwd()[:-6]
+setting = jsonLoad(f'{path}/Resources/Back')
 serra = Bot(command_prefix = '', intents = Intents.all())
-token = 'ODY4MDE2OTIyNjM2MjAyMDM2.YPphwQ.a1C0RaB0ozH3rfmuOkBPljVUYbE'
+token = 'ODY4MDE2OTIyNjM2MjAyMDM2.YPphwQ.1ajuE09cOuICGvF9_P_WPCS_afg'
 
 # >
 
 
-@serra.command(aliases = [])
-async def addFiber(ctx, arg):
-    '''  '''
+@serra.command(aliases = ['Add', 'add'])
+async def addFunction(ctx, *args):
+    ''' args[0] : str
+        args[1] : int '''
 
-    pass
+    for key, value in setting['wordBank'].items():
 
+        if (args[0] in value):
 
-@serra.command(aliases = [])
-async def addSugar(ctx, arg):
-    '''  '''
+            data = jsonLoad(f'{path}/Data/{ctx.author}')
+            data[0][key] + int(args[1])
 
-    pass
-
-
-@serra.command(aliases = [])
-async def addCalorie(ctx, arg):
-    '''  '''
-
-    pass
+            await ctx.author.send(f'{key} : {data[0][key]}', delete_after = 60)
+            await jsonDump(f'{path}/Data/{ctx.author}', data)
 
 
-@serra.command(aliases = [])
-async def addCarbohydrate(ctx, arg):
-    '''  '''
+@serra.command(aliases = ['weight', 'Weight'])
+async def weightFunction(ctx, arg):
+    ''' arg : float '''
 
-    pass
+    data = jsonLoad(f'{path}/Data/{ctx.author}')
+    today = [{'Fiber' : 0,
+              'Sugar' : 0,
+              'Calories' : 0,
+              'Carbohydrates' : 0,
+              'Weight' : float(arg),
+              'Date' : f'{dt.now().day}-{dt.now().month}-{dt.now().year}'}]
 
-
-@serra.command(aliases = [])
-async def addWeight(ctx, arg):
-    '''  '''
-
-    pass
-
+    [today.append(i) for i in data]
 
 # Run <
 
